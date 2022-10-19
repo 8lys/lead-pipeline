e0cada18dd74b3bfec54f3d33b48d83423b9f7ac
Basis <github@basis.sh>
2022-10-19 07:38:44 +0000

    Update 2 files

--- crawlee-boilerplate/src/main.ts
+++ crawlee-boilerplate/src/main.ts
@@ -1,15 +1,17 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
-import { router } from './routes.js';
+import { PlaywrightCrawler, ProxyConfiguration } from "crawlee";
+import { router } from "./routes.js";
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ["https://crawlee.dev"];
 
 const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
 });
 
-await crawler.run([{
-    url: 'http://dnr.alaska.gov',
-    label: 'docSearch'
-}]);
+await crawler.run([
+  {
+    url: "http://dnr.alaska.gov/ssd/recoff/search/indexmenu",
+    label: "docSearch",
+  },
+]);
