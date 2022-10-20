95593fceb7f7d6cb4c5d2415cded6c2f90f4131f
Basis <github@basis.sh>
2022-10-20 10:59:55 -0800

    Draft/hopeful-liskov (#1)

--- crawlee-boilerplate/src/main.ts
+++ crawlee-boilerplate/src/main.ts
@@ -1,15 +1,21 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
-import { router } from './routes.js';
-
-const startUrls = ['https://crawlee.dev'];
+import { PlaywrightCrawler, ProxyConfiguration } from "crawlee";
+import { router } from "./routes.js";
 
 const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  launchContext: {
+    launchOptions: {
+      headless: false,
+      slowMo: 150,
+    }
+  }
 });
 
-await crawler.run([{
-    url: 'http://dnr.alaska.gov',
-    label: 'docSearch'
-}]);
+await crawler.run([
+  {
+    url: "http://dnr.alaska.gov/ssd/recoff/search/indexmenu",
+    label: "documentTypeSearch"
+  },
+]);
--- crawlee-boilerplate/src/routes.ts
+++ crawlee-boilerplate/src/routes.ts
@@ -2,20 +2,60 @@ import { Dataset, createPlaywrightRouter } from 'crawlee';
 
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
+router.addDefaultHandler(async ({ log }) => {
+    log.debug(`Router: Default Handler`);
+    // await enqueueLinks({
+    //     globs: ['https://crawlee.dev/**'],
+    //     label: 'detail',
+    // });
 });
 
-router.addHandler('detail', async ({ request, page, log }) => {
-    const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
+router.addHandler('documentTypeSearch', async ({ request, page, log }) => {
+    log.info('Document Type Search initiated');
 
-    await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
+    const DISTRICT: string = '301';
+    const DOCUMENT_TYPE: string = 'M' //'M' is the index code for Mortgage
+    const SEARCH_FROM_DATE: string = new Date(Date.now() - 7776000000).toLocaleDateString();
+
+    // const submitForm = await page.evaluate(([DISTRICT, DOCUMENT_TYPE, SEARCH_FROM_DATE]) => {
+    //   document.getElementById('District').value = DISTRICT;
+    //   document.getElementById('IndexCode').value = DOCUMENT_TYPE;
+    //   document.getElementById('datepicker').value = SEARCH_FROM_DATE;
+    //   document.getElementsByName('FORM').submit();
+    // }, [DISTRICT, DOCUMENT_TYPE, SEARCH_FROM_DATE]);
+
+    await page.selectOption('#District', DISTRICT);
+    await page.selectOption('#IndexCode', DOCUMENT_TYPE);
+    await page.type('#datepicker', SEARCH_FROM_DATE);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('button[name="Document Type Search"]')
+    ]);
+
+    // Obtain and print list of search results
+    const results: {url: string, desc: string}[] = await page.$$eval('#indexdocs table tbody tr', (records) => {
+        let result: [] = [];
+        let items = document.querySelectorAll('#indexdocs table tbody tr');
+        records.forEach((item) => {
+            let collection = item.getElementsByTagName('td');
+            let data: (string | null)[] = Array.from(collection, (element) => {
+                return (element.firstElementChild?.hasAttribute('href')) ? element.firstElementChild.getAttribute('href') : element.innerText;
+            });
+            if (!(null === data[3]) && !(null === data[0]) && data[3].includes('NTC OF DEFAULT')) {
+                results.push({
+                    url: data[0],
+                    desc: data[3]
+                });
+            }
+        });
+        return results;
     });
+
+    log.info('Results:', results);
+
+    // Store data in default dataset
+    await Dataset.pushData(results);
+    
+    log.info('request', request);
 });
